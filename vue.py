a94026403eda46e99baf2d48bff3134dc6ac8072
Ksenija Dervanova <ksenijad99@gmail.com>
2023-02-27 19:28:25 +0100

    fixing some mistakes

--- vue.py
+++ vue.py
@@ -24,11 +24,13 @@ class Application(Tk):
                                      command=self.display_something)
         self.bouton = Button(self, text="Quitter",
                              command=self.quit_window)
-        self.bouton_add_animal = Button(self, text="Add",
+        self.bouton_add_animal = Button(self, text="Add/save changes",
                                         command=self.add_animal)
         self.btn = Button(self, text = "Delete selection",
                           command=self.controller.delete_model_animal)
         self.but = Button(self,text="Edit", command=self.edit_animal)
+
+
         # Entries
         self.search = Entry(self)
         self.entries = {}
@@ -54,6 +56,7 @@ class Application(Tk):
         self.but.pack()
         self.bouton.pack()
 
+
     def quit_window(self):
         '''To close the created GUI window'''
         self.controller.quit_window()
@@ -74,11 +77,11 @@ class Application(Tk):
         # Create empty dictionary
         dict_animal = {}
         # For each entry provided by user
-        for key in self.entries:
+        for att in self.entries:
             # Add the key and the value to the dictionary
-            dict_animal[key] = self.entries[key].get()
+            dict_animal[att] = self.entries[att].get()
             # Clear the entry fields
-            self.entries[key].delete(0, END)
+            self.entries[att].delete(0, END)
         # Execute 'add_animal' function
         # from controller, take
         # created dictionary as argument
@@ -96,9 +99,6 @@ class Application(Tk):
         '''To create a pop up window
         allowing to edit information
         about an already existing animal'''
-        # Create pop up window
-        edit_window = Toplevel(self)
-        edit_window.title("Edit animal information")
         # In entries_dict stock information
         # from the model for chosen animal
         entries_dict = self.controller.get_model_things()
@@ -107,19 +107,9 @@ class Application(Tk):
                 # if the key in entries_dict is the same
                 # as the key in self.attributes
                 if k == att:
-                    self.entries[att] = Entry(edit_window)
-                    self.entries_label[att] = Label(edit_window, text=att)
                     # Fill the entry field with information
                     # stocked in entries_dict
                     self.entries[att].insert(1, entries_dict[k])
-                    self.entries_label[att].pack()
-                    self.entries[att].pack()
-        # Create button that will apply changes and
-        # close the window once done
-        edit_window.apply_button = Button(edit_window, text="Apply",
-                                          command=lambda: [self.add_animal(),
-                                                           edit_window.destroy()])
-        edit_window.apply_button.pack()
 
     def update_list(self):
         '''To update the list displayed in the window'''
